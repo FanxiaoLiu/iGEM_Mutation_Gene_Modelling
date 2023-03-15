241ec22ad935f85ee2d8427b205c313aa12aa280
Franky Liu <fl_lfx@outlook.com>
2023-03-15 10:29:15 -0400

    Added equations, can't solve yet

--- main.py
+++ main.py
@@ -14,12 +14,56 @@ abc = [8.5,4,12]
 big_array = [a,b,c,ab,ac,bc,abc]
 error = [8,0.5,0.5]
 
-
 for x in big_array:
     for y in range(len(x)):
         x[y] -= error[y]
         if x[y] < 0:
             x[y] = 0
+
+# importing library sympy
+from sympy import symbols, Eq, solve
+  
+# defining symbols used in equations
+# or unknown variables
+alpha1,alpha2,alpha3,beta1,y2,n3,y1,beta2,beta3,n1,n2,y3 = symbols('alpha1,alpha2,alpha3,beta1,y2,n3,y1,beta2,beta3,n1,n2,y3')
+  
+# defining equations
+eq1 = Eq((alpha1 + beta1*0 - y1*0 - n1*0), 8)
+eq2 = Eq((alpha2 + y2*0 - beta2*0 - n2*0), 0.5)
+eq3 = Eq((alpha3 + n3*0 - beta3*0 - y3*0), 0.5)
+
+eq4 = Eq((alpha1 + beta1*1 - y1*0 - n1*0), 29)
+#eq5 = Eq((alpha2 + y2*0 - beta2*1 - n2*0), 0)
+#eq6 = Eq((alpha3 + n3*0 - beta3*1 - y3*0), 0)
+
+eq7 = Eq((alpha1 + beta1*0 - y1*1 - n1*0), 2)
+eq8 = Eq((alpha2 + y2*1 - beta2*0 - n2*0), 10)
+#eq9 = Eq((alpha3 + n3*0 - beta3*0 - y3*0), 1)
+
+eq10 = Eq((alpha1 + beta1*0 - y1*0 - n1*1), 4)
+#eq11 = Eq((alpha2 + y2*0 - beta2*0 - n2*0), 0)
+eq12 = Eq((alpha3 + n3*1 - beta3*0 - y3*0), 16)
+
+eq13 = Eq((alpha1 + beta1*1 - y1*1 - n1*0), 12)
+eq14 = Eq((alpha2 + y2*1 - beta2*1 - n2*0), 5)
+#eq15 = Eq((alpha3 + n3*0 - beta3*0 - y3*0), 0)
+
+eq16 = Eq((alpha1 + beta1*1 - y1*0 - n1*1), 28)
+#eq17 = Eq((alpha2 + y2*0 - beta2*0 - n2*0), 0)
+eq18 = Eq((alpha3 + n3*1 - beta3*1 - y3*0), 11.5)
+
+#eq19 = Eq((alpha1 + beta1*0 - y1*0 - n1*0), 0)
+eq20 = Eq((alpha2 + y2*1 - beta2*0 - n2*1), 8)
+eq21 = Eq((alpha3 + n3*1 - beta3*0 - y3*1), 11)
+
+eq22 = Eq((alpha1 + beta1*1 - y1*1 - n1*1), 8.5)
+eq23 = Eq((alpha2 + y2*1 - beta2*1 - n2*1), 4)
+eq24 = Eq((alpha3 + n3*1 - beta3*1 - y3*1), 12)
+
+#a_signal = alpha1 + beta1*x1 - y1*x2 - n1*x3 - 8
+#b_signal = alpha2 + y2*x2 - beta2*x1 - n2*x3 - 0.5
+#c_signal = alpha3 + n3*x3 - beta3*x1 - y3*x2 - 0.5
+
 
 big_array[0].insert(0,"A")
 big_array[1].insert(0,"B")
